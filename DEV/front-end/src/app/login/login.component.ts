6050759646ea69fb1fce7b21d5c283e53363ab9a
Mr.Viana <rafaelgov95@gmail.com>
2017-07-07 02:55:56 -0400

    Update

--- DEV/front-end/src/app/login/login.component.ts
+++ DEV/front-end/src/app/login/login.component.ts
@@ -16,15 +16,11 @@ import { AlertService } from '../shared/services/alert.service';
 })
 export class LoginComponent implements OnInit {
 
-    model: any = {};
-    loading = false;
+    alert =true;
     returnUrl: string;
-    loginActive = true;
-    legout=true;
     UserForm: FormGroup;
-    user = new LoginUser(18, 'Dr. WhatIsHisName', 'senha');
+    user:LoginUser;
     submitted = false;
-    active = true;
 
   constructor(
         private fb: FormBuilder,
@@ -37,26 +33,6 @@ export class LoginComponent implements OnInit {
       this.user = new LoginUser(0, '', '');
     }
 
- 
-
- 
-  onSubmit() {
-    this.submitted = true;
-    this.user = this.UserForm.value;
-  }
-
-  addUser() {
-    // this.user = new LoginUser(42, '', '');
-    this.buildForm();
-
-    this.active = false;
-    setTimeout(() => this.active = true, 0);
-  }
-  
-  AlertaErro(){
-    setTimeout(() => this.active = true, 0);
-  }
- 
    ngOnInit(): void {
     this.buildForm();
         // reset login status
@@ -65,34 +41,30 @@ export class LoginComponent implements OnInit {
         // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || 'dashboard';
   }
+
    buildForm(): void {
     this.UserForm = this.fb.group({
       'email': [this.user.email, [
           Validators.required,
           Validators.minLength(4),
-          // Validators.maxLength(24)
-        //   ,
-        //   forbiddenNameValidator(/bob/i)
         ]
       ],
       'senha': [this.user.senha,[
             Validators.required,
             Validators.minLength(4)
            ]]
-    //   ,
-    //   'power':    [this.hero.power, Validators.required]
     });
+
     this.UserForm.valueChanges
       .subscribe(data => this.onValueChanged(data));
 
-    this.onValueChanged(); // (re)set validation messages now
+    this.onValueChanged(); 
   }
 
    onValueChanged(data?: any) {
     if (!this.UserForm) { return; }
     const form = this.UserForm;
-    this.model.email=form.get('email').value    
-    this.model.senha=form.get('senha').value 
+    
     for (const field in this.formErrors) {
       // clear previous error message (if any)
       this.formErrors[field] = '';
@@ -106,6 +78,7 @@ export class LoginComponent implements OnInit {
       }
     }
   }
+
    formErrors = {
     'email': '',
     'senha': ''
@@ -116,33 +89,17 @@ export class LoginComponent implements OnInit {
       'required':      'Name is required.',
       'minlength':     'Name must be at least 4 characters long.',
       'maxlength':     'Name cannot be more than 24 characters long.'
-    //   'forbiddenName': 'Someone named "Bob" cannot be a hero.'
     },
     'senha': {
       'required': 'Power is required.'
     }
   };
-    
-  
-
-
-
-
-
-
-
-
-
-    // //  @Output() legout=false;
-    // //  @Output() modouValor = new EventEmitter();
 
     login() {
-        
-        this.loading = true;
-        this.authenticationService.login(this.model.email, this.model.senha)
+ 
+        this.authenticationService.login(this.UserForm.get('email').value, this.UserForm.get('senha').value)
             .subscribe(
                 data => {
-                    console.log(this.loginActive);
                      localStorage.setItem('isLoggedin', 'true');
                      this.router.navigate([this.returnUrl]);
                              
@@ -152,7 +109,8 @@ export class LoginComponent implements OnInit {
                 error => {
                     console.log('Erro');
                     this.alertService.error(error);
-                        setTimeout(() => this.active = true, 0);
+                    this.alert=false;
+                    setTimeout(() => this.alert = true, 10000);
 
                 });
     }
