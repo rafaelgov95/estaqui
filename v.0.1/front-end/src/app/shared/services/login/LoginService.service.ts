bd59c2a355c254cbb0d74e18731005fdb02151c2
Mr.Viana <rafaelgov95@gmail.com>
2017-07-12 13:53:27 -0400

    Update

--- v.0.1/front-end/src/app/shared/services/login/LoginService.service.ts
+++ v.0.1/front-end/src/app/shared/services/login/LoginService.service.ts
@@ -14,7 +14,7 @@ export class LoginService {
        urlSearchParams.append('email', email);
         urlSearchParams.append('senha', senha);
         let body = urlSearchParams.toString()
-        return this.http.post('/autentica', body, { headers: headers })
+        return this.http.post('/api/autentica', body, { headers: headers })
             .map((response: Response) => {
 
                 let body = response.json();
