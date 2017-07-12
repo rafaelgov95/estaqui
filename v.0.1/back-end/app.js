09296d853be61005b922c1bba7186f80fec7c616
Mr.Viana <rafaelgov95@gmail.com>
2017-07-12 14:15:19 -0400

    Update

--- v.0.1/back-end/app.js
+++ v.0.1/back-end/app.js
@@ -46,7 +46,7 @@ app.use('/api/*', function (req, res, next) {
     //     res.render(path.join(__dirname + '/views/dist/index.html'));
     //     //__dirname : It will resolve to your project folder.
     // });
-    // next()
+    next()
 })
 
 // app.use(function (req, res, next) {
@@ -54,7 +54,7 @@ app.use('/api/*', function (req, res, next) {
         res.render(path.join(__dirname + '/views/dist/index.html'));
         //__dirname : It will resolve to your project folder.
          // next()
-            // next()
+            next()
     });
     // next()
 // })
