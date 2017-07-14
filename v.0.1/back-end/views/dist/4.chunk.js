d13cc1dab663c14557a52ee11edea8953377d5ee
Mr.Viana <rafaelgov95@gmail.com>
2017-07-13 20:14:12 -0400

    Update

---
+++ v.0.1/back-end/views/dist/4.chunk.js
@@ -0,0 +1,263 @@
+webpackJsonp([4],{
+
+/***/ "../../../../../src/app/layout/profile/profile-routing.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__profile_component__ = __webpack_require__("../../../../../src/app/layout/profile/profile.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ProfileRoutingModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var routes = [
+    { path: '', component: __WEBPACK_IMPORTED_MODULE_2__profile_component__["a" /* ProfileComponent */] }
+];
+var ProfileRoutingModule = (function () {
+    function ProfileRoutingModule() {
+    }
+    return ProfileRoutingModule;
+}());
+ProfileRoutingModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+        imports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* RouterModule */].forChild(routes)],
+        exports: [__WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* RouterModule */]]
+    })
+], ProfileRoutingModule);
+
+//# sourceMappingURL=profile-routing.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/profile/profile.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ".profile-area img {\n  max-width: 150px;\n  margin-bottom: 20px;\n}\n\n.panel-body h3 {\n  margin-top: 0;\n}", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/profile/profile.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<app-page-header-opcoes [heading]=\"'Perfil'\" [icon]=\"'fa-table'\"></app-page-header-opcoes>\n\n<div class=\"panel panel-default profile-area\">\n    <div class=\"panel-heading\">\n        <h3>Profile</h3>\n    </div>\n    <div class=\"panel-body\">\n        <div>\n            <label><i class=\"glyphicon glyphicon-user\"></i> Nickname</label>\n            <h3 class=\"nickname\">{{ profile.nome }}</h3>\n        </div>\n    </div>\n    <a>Email: {{profile.email}}</a>\n        <a> Criada em:  {{profile.criada_em}}</a>\n    </div>\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/profile/profile.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ProfileComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var ProfileComponent = (function () {
+    function ProfileComponent() {
+        this.profile = JSON.parse(localStorage.getItem("currentUser"));
+    }
+    ProfileComponent.prototype.ngOnInit = function () {
+        // if (this.auth.userProfile) {
+        //   this.profile = this.auth.userProfile;
+        // } else {
+        //   this.auth.getProfile((err, profile) => {
+        //     this.profile = profile;
+        //   });
+        // }
+    };
+    return ProfileComponent;
+}());
+ProfileComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+        selector: 'app-profile',
+        template: __webpack_require__("../../../../../src/app/layout/profile/profile.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/layout/profile/profile.component.css")]
+    }),
+    __metadata("design:paramtypes", [])
+], ProfileComponent);
+
+//# sourceMappingURL=profile.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/layout/profile/profile.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__("../../../common/@angular/common.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/@angular/forms.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__shared_modules_page_header_opcoes_page_header_opcoes_module__ = __webpack_require__("../../../../../src/app/shared/modules/page-header-opcoes/page-header-opcoes.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__profile_routing_module__ = __webpack_require__("../../../../../src/app/layout/profile/profile-routing.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__profile_component__ = __webpack_require__("../../../../../src/app/layout/profile/profile.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ProfileModule", function() { return ProfileModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+var ProfileModule = (function () {
+    function ProfileModule() {
+    }
+    return ProfileModule;
+}());
+ProfileModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1__angular_common__["h" /* CommonModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_4__profile_routing_module__["a" /* ProfileRoutingModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["b" /* ReactiveFormsModule */],
+            // PageHeaderModule,
+            __WEBPACK_IMPORTED_MODULE_3__shared_modules_page_header_opcoes_page_header_opcoes_module__["a" /* PageHeaderOpcoesModule */]
+        ],
+        providers: [],
+        declarations: [__WEBPACK_IMPORTED_MODULE_5__profile_component__["a" /* ProfileComponent */]],
+        exports: [__WEBPACK_IMPORTED_MODULE_5__profile_component__["a" /* ProfileComponent */]]
+    })
+], ProfileModule);
+
+//# sourceMappingURL=profile.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/shared/modules/page-header-opcoes/page-header-opcoes.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"row\">\n    <div class=\"col-xl-12\">\n        <h2 class=\"page-header\">\n            {{heading}}\n        </h2>\n        <ol class=\"breadcrumb\">\n            <li class=\"breadcrumb-item\">\n                <i class=\"fa fa-dashboard\"></i> <a>Opções</a>\n            </li>\n            <li class=\"breadcrumb-item active\"><i class=\"fa {{icon}}\"></i> {{heading}}</li>\n        </ol>\n    </div>\n</div>"
+
+/***/ }),
+
+/***/ "../../../../../src/app/shared/modules/page-header-opcoes/page-header-opcoes.component.scss":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/shared/modules/page-header-opcoes/page-header-opcoes.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return PageHeaderOpcoesComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var PageHeaderOpcoesComponent = (function () {
+    function PageHeaderOpcoesComponent() {
+    }
+    return PageHeaderOpcoesComponent;
+}());
+__decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Input"])(),
+    __metadata("design:type", String)
+], PageHeaderOpcoesComponent.prototype, "heading", void 0);
+__decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Input"])(),
+    __metadata("design:type", String)
+], PageHeaderOpcoesComponent.prototype, "icon", void 0);
+PageHeaderOpcoesComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
+        selector: 'app-page-header-opcoes',
+        template: __webpack_require__("../../../../../src/app/shared/modules/page-header-opcoes/page-header-opcoes.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/shared/modules/page-header-opcoes/page-header-opcoes.component.scss")]
+    })
+], PageHeaderOpcoesComponent);
+
+//# sourceMappingURL=page-header-opcoes.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/shared/modules/page-header-opcoes/page-header-opcoes.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_common__ = __webpack_require__("../../../common/@angular/common.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__page_header_opcoes_component__ = __webpack_require__("../../../../../src/app/shared/modules/page-header-opcoes/page-header-opcoes.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return PageHeaderOpcoesModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+var PageHeaderOpcoesModule = (function () {
+    function PageHeaderOpcoesModule() {
+    }
+    return PageHeaderOpcoesModule;
+}());
+PageHeaderOpcoesModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["NgModule"])({
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1__angular_common__["h" /* CommonModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_router__["b" /* RouterModule */]
+        ],
+        declarations: [__WEBPACK_IMPORTED_MODULE_3__page_header_opcoes_component__["a" /* PageHeaderOpcoesComponent */]],
+        exports: [__WEBPACK_IMPORTED_MODULE_3__page_header_opcoes_component__["a" /* PageHeaderOpcoesComponent */]]
+    })
+], PageHeaderOpcoesModule);
+
+//# sourceMappingURL=page-header-opcoes.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=4.chunk.js.map
