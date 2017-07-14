743dd087ad8ddaff1e0d48e3c875dad2be10ca1e
Mr.Viana <rafaelgov95@gmail.com>
2017-07-13 20:12:09 -0400

    Update

--- v.0.1/front-end/src/app/layout/profile/profile.component.ts
+++ v.0.1/front-end/src/app/layout/profile/profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 // import { AuthService } from './../auth/auth.service';
 import { Router, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-s
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
