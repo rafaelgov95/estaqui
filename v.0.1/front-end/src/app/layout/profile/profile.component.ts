77aa9973e912c8fa252a59023c68e462d6006622
higorc <higor_oliveira@outlook.com>
2017-08-10 18:29:03 -0400

    Atualização front perfil

--- v.0.1/front-end/src/app/layout/profile/profile.component.ts
+++ v.0.1/front-end/src/app/layout/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 // import { AuthService } from './../auth/auth.service';
 import { Router, NavigationEnd } from '@angular/router';
@@ -10,20 +11,32 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class ProfileComponent implements OnInit {
 
+
+
   profile: any;
 
-    constructor() {
-        this.profile = JSON.parse(localStorage.getItem("currentUser"));
-    }
+  constructor() {
+    this.profile = JSON.parse(localStorage.getItem("currentUser"));
 
+  }
+
+  
   ngOnInit() {
-    // if (this.auth.userProfile) {
-    //   this.profile = this.auth.userProfile;
-    // } else {
-    //   this.auth.getProfile((err, profile) => {
-    //     this.profile = profile;
-    //   });
-    // }
+    var imagem = document.querySelector(".imagem");
+    imagem.addEventListener("click", function () {
+      alert("Esta funcionalidade ainda não foi implementada.");
+    });
+
+    var botaoFeedback = document.querySelector(".enviarFeedback");
+    var textArea = document.querySelector(".textArea");
+
+
+    botaoFeedback.addEventListener("click", function () {
+      alert("Sugestão enviada com sucesso!");
+      textArea.textContent="lae,,e";
+     
+    });
+
   }
 
 }
