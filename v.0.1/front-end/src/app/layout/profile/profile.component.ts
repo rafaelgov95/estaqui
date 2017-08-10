d061e9a1fe6c8f18a3261f4767c80537dc6c8225
Mr.Viana <rafaelgov95@gmail.com>
2017-08-10 18:51:37 -0400

    up

--- v.0.1/front-end/src/app/layout/profile/profile.component.ts
+++ v.0.1/front-end/src/app/layout/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 // import { AuthService } from './../auth/auth.service';
 import { Router, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import {MdDialog, MdDialogRef} from '@angular/material';
 
 @Component({
   selector: 'app-profile',
@@ -10,12 +11,14 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  selectedOption: string;
 
 
 
   profile: any;
 
-  constructor() {
+  constructor(public dialog: MdDialog) {
+
     this.profile = JSON.parse(localStorage.getItem("currentUser"));
 
   }
@@ -38,5 +41,20 @@ export class ProfileComponent implements OnInit {
     });
 
   }
-
+   openDialog() {
+    let dialogRef = this.dialog.open(DialogResultExampleDialog);
+    dialogRef.afterClosed().subscribe(result => {
+      this.selectedOption = result;
+    });
+  }
 }
+
+
+@Component({
+  // selector: 'dialog-result-example-dialog',
+  // templateUrl: 'dialog-result-example-dialog.html',
+})
+export class DialogResultExampleDialog {
+  constructor(public dialogRef: MdDialogRef<DialogResultExampleDialog>) {}
+}
+
